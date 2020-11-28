ab8177a03fd1640c34c42c00c077f56a178f6006
wfz <wtr0@qq.com>
2020-11-28 13:07:09 +0800

    version 0.0.2

--- src/WTRCompress.m
+++ src/WTRCompress.m
@@ -316,12 +316,9 @@
         NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingMacChineseSimp);
         retstr = [[NSString alloc] initWithData:da encoding:enc];
     }
+    
     if (!retstr) {
-        NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingHZ_GB_2312);
-        retstr = [[NSString alloc] initWithData:da encoding:enc];
-    }
-    if (!retstr) {
-        NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingISO_2022_CN);
+        NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingDOSChineseTrad);
         retstr = [[NSString alloc] initWithData:da encoding:enc];
     }
     if (!retstr) {
@@ -329,7 +326,25 @@
         retstr = [[NSString alloc] initWithData:da encoding:enc];
     }
     if (!retstr) {
-        NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingDOSChineseTrad);
+        NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingISO_2022_CN);
+        retstr = [[NSString alloc] initWithData:da encoding:enc];
+    }
+    //判断BOM
+    /*
+     Unicode
+        在UTF-16（小端）中BOM为FF FE，UTF-16（大端）中BOM为FE FF。这个其实很好记，大端还有另外一个名字叫大尾端（小端同理），FF显然比FE大，所以在大尾端中它的尾巴是FF，即大端BOM为FEFF。
+     
+     　　在UTF-32中，小端BOM为FFFE0000，大端BOM为0000FEFF。
+
+     　　在UTF-8带BOM的版本中，BOM为EF BB BF。
+     */
+    if (!retstr) {
+        //这里会自动判断 BOM Unicode
+        retstr = [[NSString alloc] initWithData:da encoding:NSUnicodeStringEncoding];;
+    }
+    
+    if (!retstr) {
+        NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingHZ_GB_2312);
         retstr = [[NSString alloc] initWithData:da encoding:enc];
     }
     if (!retstr) {
